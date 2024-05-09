4fc9ec77612955cfd17709bcbea2d4aa3a0eead3
Denis Bikkuzin <bikkuzin.den@gmail.com>
2024-05-09 20:47:08 +0500

    HelloWorld Spring!

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String firstVallue;
+
+    public String getVirstVallue() {
+        return firstVallue;
+    }
+
+    public void setVirstVallue(String virstVallue) {
+        this.firstVallue = virstVallue;
+    }
+}
